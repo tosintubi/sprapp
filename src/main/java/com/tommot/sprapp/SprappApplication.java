854bf858b2c3b28ccd775f24e03201c75ac15ccb
Tosin <{ID}+{username}@users.noreply.github.com>
2021-02-01 00:14:23 +0100

    refractoring studentAPI

--- src/main/java/com/tommot/sprapp/SprappApplication.java
+++ src/main/java/com/tommot/sprapp/SprappApplication.java
@@ -1,33 +1,15 @@
 package com.tommot.sprapp;
 
-import com.tommot.sprapp.models.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SprappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprappApplication.class, args);
 	}
 
-	@GetMapping("/greet")
-	public List<Student> greet(){
-		LocalDate dob= LocalDate.of(1970, Month.JUNE,5);
-		return List.of(new Student(
-				1L,
-				"John",
-				"Doe",
-				LocalDate.now().getYear()-dob.getYear(),
-				dob,
-				"john.doe@gmail.com"
-		));
-	}
+
 }
--- src/main/java/com/tommot/sprapp/controllers/StudentController.java
+++ src/main/java/com/tommot/sprapp/controllers/StudentController.java
@@ -1,4 +1,26 @@
 package com.tommot.sprapp.controllers;
 
+import com.tommot.sprapp.models.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Controller
 public class StudentController {
+
+    @GetMapping("/greet")
+    public List<Student> greet(){
+        LocalDate dob= LocalDate.of(1970, Month.JUNE,5);
+        return List.of(new Student(
+                1L,
+                "John",
+                "Doe",
+                LocalDate.now().getYear()-dob.getYear(),
+                dob,
+                "john.doe@gmail.com"
+        ));
+    }
 }
