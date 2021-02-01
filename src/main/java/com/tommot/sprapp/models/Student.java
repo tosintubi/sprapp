c75474fba54238466d39c9339cad87851f73b4d9
Tosin <{ID}+{username}@users.noreply.github.com>
2021-02-01 01:16:46 +0100

    @Entity sequence generator

--- src/main/java/com/tommot/sprapp/models/Student.java
+++ src/main/java/com/tommot/sprapp/models/Student.java
@@ -1,9 +1,24 @@
 package com.tommot.sprapp.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+            
+    )
     private Long id;
     private String firstName;
     private String lastName;
